ec408bc08d095475d7c839180330bba672d54572
Dhruv Thakur <dhruvt93@gmail.com>
2016-10-10 07:07:53 +0000

    Added code to print out file matrix

--- tensorflow/main.py
+++ tensorflow/main.py
@@ -3,14 +3,23 @@ from collections import Counter
 import csv
 import os
 
-fnames = []
+f_names = []
+f_types = {}
 categories = ["public", "internal", "restricted", "highly_restricted"]
 for category in categories:
     directory = category
     filenames = os.listdir(directory)
     for file in filenames:
-        f_name=os.path.join(directory, file)
-        fnames.append(f_name)
+        f_name = os.path.join(directory, file)
+        f_names.append(f_name)
+        if category == "public":
+            f_types[f_name] = "p"
+        elif category == "internal":
+            f_types[f_name] = "i"
+        elif category == "restricted":
+            f_types[f_name] = "r"
+        else:
+            f_types[f_name] = "h"
         # f = open(f_name)
         # print f.read()
         # f.close()
@@ -19,7 +28,7 @@ for category in categories:
 # restricted = []
 # highly_restricted = []
 #
-# fnames= ['data2','data3','data4', 'data3']
+# f_names= ['data2','data3','data4', 'data3']
 freqs = []
 def get_list(data):
     return re.compile('\w+').findall(data)
@@ -33,29 +42,41 @@ def get_data(fname):
 words = set()
 
 def build_set():
-    for f in fnames:
+    for f in f_names:
         l = get_list(get_data(f))
         freqs.append(Counter(l))
         words.update(l)
 
-
-
-def print_out():
-    with open("out.csv", "w") as output:
+def print_out_word_matrix():
+    with open("word_matrix.csv", "w") as output:
         writer = csv.writer(output, lineterminator='\n')
-
-
         first_row = ['file'] + list(words)
         writer.writerow(first_row)
         count = 0
-        for f in fnames:
-            row = [f]
+        for f in f_names:
+            row = [f.split("/")[0]]
             for word in words:
                 row.append(freqs[count][word])
             writer.writerow(row)
             count+=1
 
+def print_out_file_matrix():
+    with open("file_matrix.csv", "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        first_row = ["category"] + categories
+        writer.writerow(first_row)
+        for file in f_names:
+            row = [file.split("/")[0]]
+            if f_types[file] == "p":
+                row += [1,0,0,0]
+            elif f_types[file] == "i":
+                row += [0,1,0,0]
+            elif f_types[file] == "r":
+                row += [0,0,1,0]
+            else:
+                row += [0,0,0,1]
+            writer.writerow(row)
 
 build_set()
-print_out()
-
+print_out_word_matrix()
+print_out_file_matrix()
